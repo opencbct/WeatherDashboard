558909113121f68031ab0825d8eca8817bd6bf2a
Benjamin Kim <84877261+opencbct@users.noreply.github.com>
2021-07-29 11:58:54 -0500

    short

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-//create a search bar
-//when the user types a city into the search bar and clicks the search button, fetch the city info from weather api
-//if it's an invalid city, return an error (the api fetch will be unsuccessful)
-//if not an error -
-//display the city, date of forecast, and an icon for the main weather status
 
-//in smaller text, display the current day's temp, wind, humidity, and uv index (with colored background representing favorable, moderate, severe uv)
-//below the current day's display, place a display of a 5-day forecast
 //one card for each day of the forecast
 //each card displays the same info categories from the current day excepting the uv index
 //to make each card use the fetch data from the search - it responds with a 7-day forecast in an array. loop through that array for each of the 5 days needed, and add data to the card.
